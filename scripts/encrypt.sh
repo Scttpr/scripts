d4a6f063c089c110cd8d49d2adca39c4fbaac0cf
Scttpr <charles.capelli@inclusion.gouv.fr>
2025-01-25 21:52:24 +0100

    Update encrypt.sh

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -1,3 +1,10 @@
+#!/bin/sh
+
+if ! command -v openssl 2>&1 >/dev/null
+then
+    echo "openssl could not be found"
+    exit 1
+fi
+
 FILE="$1"
 openssl enc -aes-256-cbc -pbkdf2 -in "$FILE" -out "$FILE.enc"
-

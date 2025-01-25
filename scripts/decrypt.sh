e4f1cd2277e34f5b9a44603d39c85fa5d913f0de
Scttpr <charles.capelli@inclusion.gouv.fr>
2025-01-25 21:48:30 +0100

    Update decrypt.sh

--- scripts/decrypt.sh
+++ scripts/decrypt.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
+if ! command -v openssl 2>&1 >/dev/null
+then
+    echo "openssl could not be found"
+    exit 1
+fi
+
 FILE="$1"
 OUTPUT_FILE="${FILE%.enc}"
 openssl enc -aes-256-cbc -pbkdf2 -d -in "$FILE" -out "$OUTPUT_FILE"
-
